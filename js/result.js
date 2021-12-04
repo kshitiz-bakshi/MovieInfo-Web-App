73e63cae7f229724fd1fce9a8f2606fa28c2a4e8
kshitiz bakshi <bakshikshitiz@gmail.com>
2021-12-04 18:24:50 +0530

    Final commit

---
+++ js/result.js
@@ -0,0 +1,52 @@
+const key = "e1e16497";
+const apiurl = "http://www.omdbapi.com/?apikey=" + key;
+const title = localStorage.getItem("Title");
+var searchUrl = apiurl + "&t=" + title;
+searchUrl = searchUrl.replace(' ', '+');
+localStorage.removeItem("Title");
+
+getdata(searchUrl);
+async function getdata(url) {
+    console.log(url)
+    const response = await fetch(url);
+    const data = await response.json();
+
+    var res = data.Response;
+    console.log(res);
+    const div = document.createElement('div');
+    const image = document.createElement('img');
+    const text = document.createElement('h1');
+
+    div.style.color = "white";
+    div.setAttribute('class','image');
+    image.src = data.Poster;
+    div.appendChild(text);
+    div.appendChild(image);
+    text.innerHTML = data.Title;
+    console.log(data.Title);
+    document.getElementById("main").appendChild(div);
+
+
+    const a = document.getElementById('a');
+    a.innerHTML='<b>Genre</b> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: ' + data.Genre;
+    const b = document.getElementById('b');
+    b.innerHTML='<b>Released </b>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: ' + data.Released;
+    const c = document.getElementById('c');
+    c.innerHTML='<b>Rated</b> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: ' + data.Rated;
+    const d = document.getElementById('d');
+    d.innerHTML='<b>imdbRating</b> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: ' + data.imdbRating;
+    const e = document.getElementById('e');
+    e.innerHTML='<b>Director</b> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: ' + data.Director;
+    const f = document.getElementById('f');
+    f.innerHTML='<b>Writer</b> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: ' + data.Writer;
+    const g = document.getElementById('g');
+    g.innerHTML='<b>Actors</b> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: ' + data.Actors;
+
+    const plot = document.getElementById('plot');
+    plot.innerHTML="<b>Plot : </b>" + data.Plot;
+
+    const anc = document.getElementById('imdb');
+    anc.href="https://www.imdb.com/title/"+data.imdbID;
+
+
+}
