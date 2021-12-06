222bc293ea3fc2cbc3c4ad773196ff8e4ceb1cdb
kshitiz bakshi <bakshikshitiz@gmail.com>
2021-12-06 17:35:04 +0530

    Feedback commit

--- js/main.js
+++ js/main.js
@@ -1,87 +1,84 @@
 const key = "e1e16497";
-    const apiurl = "https://www.omdbapi.com/?apikey=" + key;
-    const form = document.getElementById("form");
-    const search = document.getElementById("Movie");
-    const main = document.getElementById("main");
-    var data = "";
+const apiurl = "https://www.omdbapi.com/?apikey=" + key;
+const form = document.getElementById("form");
+const search = document.getElementById("Movie");
+const main = document.getElementById("main");
+var data = "";
 
-    async function getdata(url) {
-        console.log(url)
-        const response = await fetch(url);
-        data = await response.json();
+async function getdata(url) {
+    const response = await fetch(url);
+    data = await response.json();
 
-        var res = data.Response;
+    var res = data.Response;
+    console.log(data.Response);
+    if (res == "False" && data.Error == "Movie not found!") {
+        const div = document.createElement('div')
+        div.style.width = "400px";
+        div.style.height = "30px";
+        div.style.color = "white";
+        div.innerHTML = "NO MOVIE FOUND!";
+        alert("NO MOVIE FOUND!")
 
-        console.log(data.Response);
-        if (res == "False" && data.Error == "Movie not found!") {
-            const div = document.createElement('div')
-            div.style.width = "400px";
-            div.style.height = "30px";
-            div.style.color = "white";
-            div.innerHTML = "NO MOVIE FOUND!";
-            alert("NO MOVIE FOUND!")
+        document.getElementById("main").appendChild(div);
 
-            document.getElementById("main").appendChild(div);
-
-        }
-        if (res == "False" && data.Error == "Too many results.") {
-            const div = document.createElement('div')
-            div.style.width = "400px";
-            div.style.height = "30px";
-            div.style.color = "white";
-            div.innerHTML = "Too many results....";
-            alert("Too many results....")
-
-            document.getElementById("main").appendChild(div);
-
-        }
-        else {
-            var i = 0;
-            data.Search.forEach(element => {
-                const div = document.createElement('div')
-                const anc = document.createElement('a');
-                const image = document.createElement('img');
-                div.style.color = "white";
-                div.innerHTML = element.Title;
-                image.src = element.Poster;
-                var ref = apiurl+"&t="+element.Title;
-                ref = ref.replace(' ','+');
-                // console.log(element.Title);
-                anc.href = "result.html" ;
-                i=i+1;
-                image.setAttribute('id',i);
-                div.appendChild(anc);
-                anc.appendChild(image);
-                document.getElementById("main").appendChild(div);
-            });
-        }
     }
+    if (res == "False" && data.Error == "Too many results.") {
+        const div = document.createElement('div')
+        div.style.width = "400px";
+        div.style.height = "30px";
+        div.style.color = "white";
+        div.innerHTML = "Too many results....";
+        alert("Too many results....")
 
-    var searchUrl = "";
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        main.innerHTML = '';
+        document.getElementById("main").appendChild(div);
 
-        var searchTerm = search.value;
-        searchTerm = searchTerm.replace(' ', '+');
-
-        if (searchTerm) {
-            searchUrl = apiurl + "&s=" + searchTerm
-            getdata(searchUrl);
-            search.value = "";
-        }
-    });
-
-
-    var the= document.querySelector('main');
-    the.addEventListener("click",doSomething, false);
-    
-
-    function doSomething(e){
-        if(e.target!=e.currentTarget){
-            var clicked = e.target.id;
-            localStorage.setItem("Title",data.Search[clicked-1].Title);
-
-        }
     }
-    
+    else {
+        var i = 0;
+        data.Search.forEach(element => {
+            const div = document.createElement('div')
+            const anc = document.createElement('a');
+            const image = document.createElement('img');
+            div.style.color = "white";
+            div.innerHTML = element.Title;
+            image.src = element.Poster;
+            var ref = apiurl + "&t=" + element.Title;
+            ref = ref.replace(' ', '+');
+            // console.log(element.Title);
+            anc.href = "result.html";
+            i = i + 1;
+            image.setAttribute('id', i);
+            div.appendChild(anc);
+            anc.appendChild(image);
+            document.getElementById("main").appendChild(div);
+        });
+    }
+}
+
+var searchUrl = "";
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+
+    var searchTerm = search.value;
+    searchTerm = searchTerm.replace(' ', '+');
+
+    if (searchTerm) {
+        searchUrl = apiurl + "&s=" + searchTerm
+        getdata(searchUrl);
+        search.value = "";
+    }
+});
+
+
+var the = document.querySelector('main');
+the.addEventListener("click", doSomething, false);
+
+
+function doSomething(e) {
+    if (e.target != e.currentTarget) {
+        var clicked = e.target.id;
+        localStorage.setItem("Title", data.Search[clicked - 1].Title);
+
+    }
+}
